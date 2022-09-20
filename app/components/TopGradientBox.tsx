ac44c2d39295ec0244052509c79260c78c34ff21
Ragnar Bråstein <ragnarob@protonmail.com>
2022-09-20 20:35:15 +0200

    Fix TopGradientBox

--- app/components/TopGradientBox.tsx
+++ app/components/TopGradientBox.tsx
@@ -1,16 +1,21 @@
-import clsx from 'clsx';
-
-const gradientClasses = `before:h-2.5 before:w-full before:absolute before:-top-1 before:left-0 before:content-['']
-   before:bg-gradient-to-r before:from-theme1 before:to-theme2 bg-white dark:bg-gray-200`;
-
 type TopGradientBoxProps = {
+  containerClassName?: string;
+  innerClassName?: string;
   children: React.ReactNode;
-  className?: string;
 };
 
 export default function TopGradientBox({
+  containerClassName,
+  innerClassName,
   children,
-  className = '',
 }: TopGradientBoxProps) {
-  return <div className={clsx(gradientClasses, className)}>{children}</div>;
+  return (
+    <div
+      className={`pt-2.5 bg-gradient-to-r from-theme1-primary to-theme2-primary shadow-lg ${containerClassName}`}
+    >
+      <div className={`bg-white dark:bg-gray-300 w-full h-full ${innerClassName}`}>
+        {children}
+      </div>
+    </div>
+  );
 }
