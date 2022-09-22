30aa7ad351b95e0f9701be6339a2d92f8671b19b
Garrett Watson <toast@isota.ch>
2022-09-21 20:14:37 -0400

    Fixed button disabled color + implemented LoadingButton spinner + implemented InfoBox close icon

--- app/components/Buttons/LoadingButton.tsx
+++ app/components/Buttons/LoadingButton.tsx
@@ -1,11 +1,21 @@
 import Button, { ButtonProps } from './Button';
 
-// TODO actually use isLoading to display the spinner as a start icon on the Button
-
 type LoadingButtonProps = {
   isLoading: boolean;
 } & ButtonProps;
 
 export default function LoadingButton({ isLoading, ...props }: LoadingButtonProps) {
-  return <Button {...props} />;
+  const className = (isLoading ? 'opacity-70 ' : '') + props.className;
+
+  return (
+    <Button
+      {...props}
+      startIcon={isLoading ? Spinner : props.startIcon}
+      className={className}
+    />
+  );
 }
+
+const Spinner = () => (
+  <div className="mr-3 w-4 h-4 animate-spin border-solid border-transparent border-r-white border rounded-full" />
+)
