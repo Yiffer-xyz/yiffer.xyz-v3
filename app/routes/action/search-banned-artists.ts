6586e9ea4ea3ada26974220dedeeb829642821b6
Erik <meltonaerik@gmail.com>
2022-09-25 21:11:35 +0200

    Add logic for similar comics and artists to suggest flow via actions
    Rework fetch logic to use custom actions

---
+++ app/routes/action/search-banned-artists.ts
@@ -0,0 +1,38 @@
+import type { ActionFunction } from '@remix-run/cloudflare';
+import type { Artist } from '~/types/types';
+import { json } from '@remix-run/cloudflare';
+
+type SimilarBannedArtistList = {
+  similarArtists: Array<Artist>;
+  exactMatchArtist: string | null;
+};
+
+export const action: ActionFunction = async function ({ request, context }) {
+  const urlBase = context.URL_BASE as string;
+  const body = await request.formData();
+  const artistName = body.get('artistName') as string;
+
+  if (!artistName) return json([]);
+
+  const similarBannedArtists = await getSimilarBannedArtists(urlBase, artistName);
+
+  if (similarBannedArtists.exactMatchArtist) {
+    return json({ bannedArtists: [similarBannedArtists.exactMatchArtist] });
+  } else if (similarBannedArtists.similarArtists.length > 0) {
+    return json({ bannedArtists: similarBannedArtists.similarArtists });
+  }
+
+  return json({ bannedArtists: [] });
+};
+
+async function getSimilarBannedArtists(
+  urlBase: string,
+  artistName: string
+): Promise<SimilarBannedArtistList> {
+  const response = await fetch(
+    `${urlBase}/api/similar-banned-artists?artistName=${artistName}`
+  );
+  const similarArtists: SimilarBannedArtistList = await response.json();
+
+  return similarArtists;
+}
