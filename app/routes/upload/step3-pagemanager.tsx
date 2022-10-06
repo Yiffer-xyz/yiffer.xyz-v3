1e0c7ae2571a5ddaf51263824ccc9eb07da61ce7
Ragnar Bråstein <ragnarob@protonmail.com>
2022-10-06 03:12:21 +0200

    Implement submit/error logic, as well as file uploading

--- app/routes/upload/step3-pagemanager.tsx
+++ app/routes/upload/step3-pagemanager.tsx
@@ -1,7 +1,26 @@
-export default function Step3Pagemanager({}) {
+import { NewComicData } from '.';
+
+type Step3Props = {
+  comicData: NewComicData;
+  onUpdate: (newData: NewComicData) => void;
+};
+
+// Obviously, this whole component/step is very wip.
+
+export default function Step3Pagemanager({ comicData, onUpdate }: Step3Props) {
+  function onFileChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const files = event.target.files;
+    if (files) {
+      console.log(files);
+      const fileList = Array.from(files);
+      onUpdate({ ...comicData, files: fileList });
+    }
+  }
+
   return (
     <>
       <h4 className="mt-8">Pages</h4>
+      <input type="file" onChange={onFileChange} multiple />
     </>
   );
 }
---
+++ app/routes/upload/success.tsx
@@ -0,0 +1,29 @@
+import InfoBox from '~/components/InfoBox';
+import Link from '~/components/Link';
+
+type SuccessMessageProps = {
+  isLoggedIn: boolean;
+};
+
+export default function SuccessMessage({ isLoggedIn }: SuccessMessageProps) {
+  return (
+    <InfoBox variant="success" title="Upload successful!" boldText={false}>
+      <p className="my-4">
+        Your comic has been uploaded successfully. It will be reviewed by our moderator team
+        shortly.
+      </p>
+      {isLoggedIn ? (
+        <p>
+          You can view the status of your submission at the{' '}
+          <Link href="/your-contributions" text="your contributions" /> page.
+        </p>
+      ) : (
+        <p>
+          Since you are not logged in, you cannot track the status of your submission. We recommend
+          logging in or creating a user for next time! This will also allow you to collect reward
+          points for your submissions.
+        </p>
+      )}
+    </InfoBox>
+  );
+}
