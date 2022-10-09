5f054f7a73e7b926b4782002654b425c29c9647d
Garrett Watson <toast@isota.ch>
2022-10-09 13:15:16 -0400

    Resolve merge conflict

--- app/routes/contribute/scoreboard/index.tsx
+++ app/routes/contribute/scoreboard/index.tsx
@@ -1,16 +1,222 @@
-import BackToContribute from '../BackToContribute';
+import type { ActionFunction } from '@remix-run/cloudflare';
+import { json } from '@remix-run/cloudflare';
+import { useFetcher } from '@remix-run/react';
+import { useEffect, useState } from 'react';
+import {
+  MdArrowBack,
+  MdArrowDropDown,
+  MdArrowDropUp,
+  MdArrowForward,
+  MdHome,
+} from 'react-icons/md';
+import Button from '~/components/Buttons/Button';
+import Link from '~/components/Link';
+import Username from '~/components/Username';
+import { alltimequery, defaultquery } from '~/mock-data/top-contributions';
+import BackToContribute from '~/routes/contribute/BackToContribute';
 
-export default function Upload() {
+export const action: ActionFunction = async function ({ request }) {
+  const reqBody = await request.formData();
+  const { type, month, year } = Object.fromEntries(reqBody);
+
+  const response = type === 'alltime' ? alltimequery() : defaultquery(+month, +year);
+
+  return json(response);
+};
+
+const arrowButtonClasses =
+  'dark:bg-transparent dark:hover:bg-transparent bg-transparent hover:bg-transparent';
+
+export default function Scoreboard() {
+  const fetcher = useFetcher();
+  const [showPointInfo, setShowPointInfo] = useState(false);
+  const [showAllTime, setShowAllTime] = useState(false);
+  const [date, setDate] = useState(new Date());
+
+  const incrementMonth = () => {
+    if (date.getMonth() === 11) {
+      setDate(new Date(date.getFullYear() + 1, 0));
+    } else {
+      setDate(new Date(date.getFullYear(), date.getMonth() + 1));
+    }
+  };
+
+  const decrementMonth = () => {
+    if (date.getMonth() === 0) {
+      setDate(new Date(date.getFullYear() - 1, 11));
+    } else {
+      setDate(new Date(date.getFullYear(), date.getMonth() - 1));
+    }
+  };
+
+  const canIncrementMonth = () => {
+    const now = new Date();
+    return !(
+      date.getMonth() === now.getMonth() && date.getFullYear() === now.getFullYear()
+    );
+  };
+
+  const canDecrementMonth = () => {
+    return !(date.getMonth() === 0 && date.getFullYear() === 2016);
+  };
+
+  const enabledClass = `
+    dark:text-gray-100 text-gray-100 bg-gradient-to-r from-theme1-primary to-theme2-primary
+  `;
+  const disabledClass = `
+    dark:text-white dark:bg-gray-500 dark:hover:bg-gray-300 dark:focus:bg-gray-300
+    text-white bg-gray-700 hover:bg-gray-700 focus:bg-gray-700
+  `;
+
+  useEffect(() => {
+    fetcher.submit(
+      {
+        type: showAllTime ? 'alltime' : 'default',
+        month: date.getMonth() + '',
+        year: date.getFullYear() + '',
+      },
+      { method: 'post', action: '/scoreboard?index' }
+    );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [showAllTime, date]);
+
   return (
-    <div className="container mx-auto">
-      <h1>Contributions scoreboard</h1>
-      <p className="mb-4">
+    <div className="container mx-auto justify-items-center">
+      <h1 className="text-center mb-2">Contributions scoreboard</h1>
+      <p className="text-center">
         <BackToContribute />
+      </p>
+      <p className="mb-4 text-center">
+        <Link href="https://yiffer.xyz/" text="To front page" Icon={MdHome} />
       </p>
 
       <p>
-        See the imported code for the mocked responses to the database queries for month and year to
-        use.
+        Points are awarded only when a contribution has been approved or accepted by a
+        mod.
+      </p>
+      <p className="mb-6">
+        Currently, these points can&apos;t be used for anything, and there are no concrete
+        plans to change this, but who knows what the future holds?
+      </p>
+
+      <p className="text-center">
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a
+          onClick={() => setShowPointInfo(!showPointInfo)}
+          className="w-fit h-fit text-blue-weak-200 dark:text-blue-strong-300 font-semibold"
+        >
+          {showPointInfo ? 'Hide' : 'Show'} point info{' '}
+          {showPointInfo ? <MdArrowDropUp /> : <MdArrowDropDown />}
+        </a>
+      </p>
+      {showPointInfo && (
+        <div className="flex gap-2 mt-4">
+          <div className="font-bold">
+            <p className="dark:text-green-600 text-green-700">200</p>
+            <p className="dark:text-green-600 text-green-700">150</p>
+            <p className="dark:text-green-600 text-green-700">120</p>
+            <p className="dark:text-green-600 text-green-700">50</p>
+            <p className="dark:text-blue-600 text-blue-700">30</p>
+            <p className="dark:text-blue-600 text-blue-700">15</p>
+            <p className="dark:text-purple-600 text-purple-700">10</p>
+            <p className="dark:text-yellow-500 text-yellow-600">5</p>
+          </div>
+          <div>
+            <p className="dark:text-green-400 text-green-600">
+              Uploaded comic, no issues found
+            </p>
+            <p className="dark:text-green-400 text-green-600">
+              Uploaded comic, minor issues found(incorrect category/classification, wrong
+              name)
+            </p>
+            <p className="dark:text-green-400 text-green-600">
+              Uploaded comic, major issues found (lacking artist links, poor tagging, bad
+              thumbnail)
+            </p>
+            <p className="dark:text-green-400 text-green-600">
+              Uploaded comic, page issues (resolution, ordering, premium pages uploaded)
+            </p>
+            <p className="dark:text-blue-400 text-blue-600">
+              Comic suggestion approved with good links/information
+            </p>
+            <p className="dark:text-blue-400 text-blue-600">
+              Comic suggestion approved with lacking links/information
+            </p>
+            <p className="dark:text-purple-400 text-purple-600">Comic problem reported</p>
+            <p className="dark:text-yellow-200 text-yellow-500">
+              Add/remove tag suggestion approved
+            </p>
+          </div>
+        </div>
+      )}
+      <div className="flex flex-col justify-center items-center w-fit mx-auto mt-8">
+        <div className="flex mb-6">
+          <Button
+            text="Monthly"
+            variant="contained"
+            color="primary"
+            className={
+              'rounded-r-none' + disabledClass + (showAllTime ? '' : enabledClass)
+            }
+            onClick={() => setShowAllTime(false)}
+          />
+          <Button
+            text="All time"
+            variant="contained"
+            color="primary"
+            className={
+              'rounded-l-none' + disabledClass + (showAllTime ? enabledClass : '')
+            }
+            onClick={() => setShowAllTime(true)}
+          />
+        </div>
+        {!showAllTime && (
+          <div className="flex justify-between w-full mb-2 text-lg">
+            <Button
+              startIcon={MdArrowBack}
+              color="none"
+              onClick={decrementMonth}
+              disabled={!canDecrementMonth()}
+              className={arrowButtonClasses}
+            />
+            {`${date.toLocaleString('default', {
+              month: 'short',
+            })} ${date.getFullYear()}`}
+            <Button
+              endIcon={MdArrowForward}
+              color="none"
+              onClick={incrementMonth}
+              disabled={!canIncrementMonth()}
+              className={arrowButtonClasses}
+            />
+          </div>
+        )}
+        {fetcher.data && (
+          <table>
+            <thead className="font-semibold dark:bg-black dark:bg-opacity-75">
+              <tr className="border-b border-gray-400">
+                <th className="text-left">Username</th>
+                <th className="text-left">Score</th>
+              </tr>
+            </thead>
+            <tbody className="dark:bg-black dark:bg-opacity-25 font-light">
+              {fetcher.data instanceof Array &&
+                fetcher.data.map((entry, i) => (
+                  <tr key={i} className="border-b border-gray-400">
+                    <td className="dark:text-text-dark">
+                      <Username user={entry.user} />
+                    </td>
+                    <td className="dark:text-text-dark">{entry.points}</td>
+                  </tr>
+                ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+
+      <p>
+        See the imported code for the mocked responses to the database queries for month
+        and year to use.
       </p>
     </div>
   );
