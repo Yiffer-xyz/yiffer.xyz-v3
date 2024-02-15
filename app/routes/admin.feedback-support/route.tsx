a3919b8fc81c01c5e36491cddbec41a054d5eb24
Erik Melton <meltonaerik@gmail.com>
2024-02-15 22:21:15 +0100

    User feedback page and API routes

--- app/routes/admin.feedback-support/route.tsx
+++ app/routes/admin.feedback-support/route.tsx
@@ -2,6 +2,12 @@ import type { LoaderFunctionArgs } from '@remix-run/cloudflare';
 import { useLoaderData } from '@remix-run/react';
 import { getAllFeedback } from '~/route-funcs/get-feedback';
 import { processApiError } from '~/utils/request-helpers';
+import FeedbackItem from '~/page-components/UserFeedback/FeedbackItem'
+import Button from '~/ui-components/Buttons/Button'
+import {useState} from 'react'
+import {useGoodFetcher} from '~/utils/useGoodFetcher'
+import {ArchiveFeedbackBody} from '~/routes/api.admin.archive-feedback'
+import {DeleteFeedbackBody} from '~/routes/api.admin.delete-feedback'
 
 export async function loader(args: LoaderFunctionArgs) {
   const feedbackRes = await getAllFeedback(args.context.DB);
@@ -15,16 +21,77 @@ export async function loader(args: LoaderFunctionArgs) {
 export default function AdminFeedback() {
   const { feedback } = useLoaderData<typeof loader>();
 
+  const [ unprocessedSelected, setUnprocessedSelected ] = useState(true);
+
+  const archiveFeedbackFetcher = useGoodFetcher({
+    url: '/api/admin/archive-feedback',
+    method: 'post'
+  })
+  const deleteFeedbackFetcher = useGoodFetcher({
+    url: '/api/admin/delete-feedback',
+    method: 'post'
+  })
+
+  function onArchiveClick(id: number) {
+    const body: ArchiveFeedbackBody = {
+      feedbackId: id
+    }
+
+    archiveFeedbackFetcher.submit({body: JSON.stringify(body)})
+  }
+
+  function onDeleteClick(id: number) {
+    const body: DeleteFeedbackBody = {
+      feedbackId: id
+    }
+
+    deleteFeedbackFetcher.submit({body: JSON.stringify(body)})
+  }
+
+  const feedbackToShow = unprocessedSelected ? feedback.filter(f => !f.isArchived) : feedback.filter(f => f.isArchived);
+
+  const selectedButtonClass = "bg-gradient-to-r from-[#7FC7C4] to-[#9CEDCF]";
+  const unselectedButtonClass = "bg-gray-700 dark:bg-gray-500";
+
+  let unprocessedButtonClass = 'px-4 rounded-l-md rounded-r-none';
+  let archivedButtonClass = 'px-8 rounded-r-md rounded-l-none';
+
+  if (unprocessedSelected) {
+    unprocessedButtonClass += ' ' + selectedButtonClass;
+    archivedButtonClass += ' ' + unselectedButtonClass;
+  } else {
+    unprocessedButtonClass += ' ' + unselectedButtonClass;
+    archivedButtonClass += ' ' + selectedButtonClass;
+  }
+
   return (
     <>
-      <h1>Feedback and support</h1>
-      <p className="font-bold">ℹ️ See the figma prototype.</p>
+      <h1>User feedback</h1>
+      <div className="flex flex-row my-4">
+        <Button className={unprocessedButtonClass} text="Unprocessed" onClick={() => setUnprocessedSelected(true)}/>
+        <Button className={archivedButtonClass} text="Archived" onClick={() => setUnprocessedSelected(false)}/>
+      </div>
+      <section className="max-w-2xl">
+        <p>
+          To respond to support requests, go to the user's profile and send them an email via their account's associated
+          address. Make sure to archive the issue here immediately, to prevent other mods from doing the same.
+        </p>
 
-      {feedback.map(feedback => (
-        <div key={feedback.id} className="my-4">
-          <pre>{JSON.stringify(feedback, null, 2)}</pre>
-        </div>
-      ))}
+        {feedbackToShow.map(feedback => (
+          <div key={feedback.id} className="my-4">
+            <FeedbackItem
+              id={feedback.id}
+              archived={feedback.isArchived}
+              text={feedback.text}
+              type={feedback.type}
+              user={feedback.user}
+              timestamp={feedback.timestamp}
+              onArchiveClick={onArchiveClick}
+              onDeleteClick={onDeleteClick}
+            />
+          </div>
+        ))}
+      </section>
     </>
   );
 }
---
+++ app/routes/api.admin.archive-feedback.ts
@@ -0,0 +1,24 @@
+import {parseFormJson} from '~/utils/formdata-parser'
+import {ActionFunctionArgs} from '@remix-run/cloudflare'
+import {archiveFeedback} from '~/route-funcs/archive-feedback'
+import {createSuccessJson, processApiError} from '~/utils/request-helpers'
+
+export type ArchiveFeedbackBody = {
+  feedbackId: number;
+};
+
+export async function action(args: ActionFunctionArgs) {
+  const { fields, isUnauthorized } = await parseFormJson<ArchiveFeedbackBody>(args, 'mod');
+
+  if (isUnauthorized) return new Response('Unauthorized', { status: 401 });
+
+  const err = await archiveFeedback(args.context.DB, fields.feedbackId);
+
+  if (err) {
+    return processApiError('Error archiving feedback', err, {
+      ...fields
+    });
+  }
+
+  return createSuccessJson();
+}
