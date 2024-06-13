bb07ca5bfabbcb2a21ea6478bb0e89cf5c70fed5
Ragnar Bråstein <ragnarob@protonmail.com>
2024-06-13 18:07:16 +0200

    Show comic images under raw data for fun

--- app/routes/$comicname/route.tsx
+++ app/routes/$comicname/route.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunctionArgs } from '@remix-run/cloudflare';
 import { useLoaderData } from '@remix-run/react';
 import { getComicByField } from '~/route-funcs/get-comic';
+import { padPageNumber } from '~/utils/general';
 import { processApiError } from '~/utils/request-helpers';
 
 export async function loader(args: LoaderFunctionArgs) {
@@ -19,17 +20,19 @@ export async function loader(args: LoaderFunctionArgs) {
     return {
       comic: null,
       notFound: true,
+      pagesPath: args.context.PAGES_PATH,
     };
   }
 
   return {
     comic: comic.result,
     notFound: false,
+    pagesPath: args.context.PAGES_PATH,
   };
 }
 
 export default function ComicPage() {
-  const { comic, notFound } = useLoaderData<typeof loader>();
+  const { comic, notFound, pagesPath } = useLoaderData<typeof loader>();
 
   if (notFound || !comic) {
     return <div>Comic not found</div>;
@@ -39,6 +42,17 @@ export default function ComicPage() {
     <div>
       <h1>{comic.name}</h1>
       <pre>{JSON.stringify(comic, null, 2)}</pre>
+
+      <div className="flex flex-row gap-4 flex-wrap">
+        {Array.from({ length: comic.numberOfPages }, (_, i) => (
+          <img
+            key={i}
+            src={`${pagesPath}/${comic.name}/${padPageNumber(i + 1)}.jpg`}
+            alt={`Page ${i + 1}`}
+            style={{ maxWidth: 500 }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
