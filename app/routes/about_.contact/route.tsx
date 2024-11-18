d1514379f5de171b3e9463bb01551cf53bc0d2bb
Ragnar Bråstein <ragnarob@protonmail.com>
2024-11-18 00:00:01 -0800

    Implement about > Contact and takedowns

--- app/routes/about_.contact/route.tsx
+++ app/routes/about_.contact/route.tsx
@@ -12,13 +12,32 @@ export default function ContactPage() {
         currentRoute="Contact & takedowns"
       />
 
-      <p>
-        If you need assistance with the site or have other general inquiries, please reach
-        out{' '}
-        <Link href="/contribute/feedback" text="here" isInsideParagraph showRightArrow />.
-      </p>
+      <div className="flex flex-col gap-6">
+        <p>
+          If you need assistance with the site or have other general inquiries, please
+          reach out{' '}
+          <Link
+            href="/contribute/feedback"
+            text="here"
+            isInsideParagraph
+            showRightArrow
+          />
+          . If you do not have an account and don't wish to create one, you can submit
+          inquiries via contact@yiffer.xyz.
+        </p>
 
-      <p className="mt-4">If you are an artist and asdasdasd</p>
+        <p>
+          If you are an artist and want your content taken down, we will comply with your
+          wishes. Contact us via contact@yiffer.xyz and provide proof of ownership and
+          we'll get back to you quickly.
+        </p>
+        <p>
+          In the future, we plan to let artists take control of their own comics if they
+          wish to, and publish updates themselves. Are you an artist who's interested in
+          this feature? Let us know! This is on our roadmap already, but if people show
+          interest, we might get to it quicker than intially planned.
+        </p>
+      </div>
     </div>
   );
 }
