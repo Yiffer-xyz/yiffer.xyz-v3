502de0f38bb2c9332c360fc458a879b02a288ceb
Ragnar Bråstein <ragnarob@protonmail.com>
2025-10-27 23:00:52 -0700

    Add analytics for error boundary

--- app/root.tsx
+++ app/root.tsx
@@ -325,6 +325,10 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
     stack = error.stack;
   }
 
+  posthog.capture('Error boundary hit', {
+    message: error && error instanceof Error ? error.message : 'No message',
+  });
+
   return (
     <main className="pt-16 p-4 container mx-auto">
       <h1>{message}</h1>
